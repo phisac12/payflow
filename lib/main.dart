238991d583c06b809048b65930e82f7919316781
Isaac César <isac_contest@hotmail.com>
2022-02-14 18:55:43 -0300

    Criação e pre configuração do projeto e criação de 2 telas

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/shared/themes/appcolors.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(primaryColor: AppColors.primary,),
+      home: const LoginPage(),
+    );
+  }
+}
